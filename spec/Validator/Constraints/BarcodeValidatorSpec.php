e0360c104df73cb7628e2a5e7e8edcfe38594d26
Joachim Løvgaard <joachim@loevgaard.dk>
2018-11-13 15:57:15 +0100

    added spec and fixed mapping error

---
+++ spec/Validator/Constraints/BarcodeValidatorSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Loevgaard\SyliusBarcodePlugin\Validator\Constraints;
+
+use Loevgaard\SyliusBarcodePlugin\Validator\Constraints\Barcode;
+use Loevgaard\SyliusBarcodePlugin\Validator\Constraints\BarcodeValidator;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+use Symfony\Component\Validator\Violation\ConstraintViolationBuilderInterface;
+
+class BarcodeValidatorSpec extends ObjectBehavior
+{
+    public function it_is_initializable(): void
+    {
+        $this->shouldHaveType(BarcodeValidator::class);
+    }
+
+    public function let(ExecutionContextInterface $executionContext): void
+    {
+        $this->initialize($executionContext);
+    }
+
+    public function it_validates(ExecutionContextInterface $executionContext): void
+    {
+        $executionContext->buildViolation(Argument::any())->shouldNotBeCalled();
+        $this->validate('4006381333931', new Barcode());
+    }
+
+    public function it_invalidates(ExecutionContextInterface $executionContext, ConstraintViolationBuilderInterface $constraintViolationBuilder): void
+    {
+        $value = 'invalid';
+
+        $constraintViolationBuilder->setParameter('{{ string }}', $value)->willReturn($constraintViolationBuilder)->shouldBeCalled();
+        $constraintViolationBuilder->addViolation()->shouldBeCalled();
+
+        $constraint = new Barcode();
+        $executionContext->buildViolation($constraint->message)->willReturn($constraintViolationBuilder)->shouldBeCalled();
+        $this->validate($value, $constraint);
+    }
+}
