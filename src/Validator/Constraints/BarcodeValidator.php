906fab573c8849b67d24e0b073c8f61228168552
Joachim Løvgaard <joachim@loevgaard.dk>
2019-08-13 08:17:12 +0200

    added check for correct constraint

--- src/Validator/Constraints/BarcodeValidator.php
+++ src/Validator/Constraints/BarcodeValidator.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBarcodePlugin\Validator\Constraints;
 
+use InvalidArgumentException;
+use Safe\Exceptions\StringsException;
+use function Safe\sprintf;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use violuke\Barcodes\BarcodeValidator as ActualBarcodeValidator;
 
 class BarcodeValidator extends ConstraintValidator
 {
+    /**
+     * @throws StringsException
+     */
     public function validate($value, Constraint $constraint): void
     {
         // custom constraints should ignore null and empty values to allow
@@ -17,7 +24,11 @@ class BarcodeValidator extends ConstraintValidator
             return;
         }
 
-        $validator = new \violuke\Barcodes\BarcodeValidator($value);
+        if (!$constraint instanceof Barcode) {
+            throw new InvalidArgumentException(sprintf('This validator only supports the %s constraint', Barcode::class));
+        }
+
+        $validator = new ActualBarcodeValidator($value);
         $valid = (bool) $validator->isValid();
 
         if (!$valid) {
