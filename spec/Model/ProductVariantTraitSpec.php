c8dc6cd5697f99ba934fefc0353b57d3584cc444
Joachim Løvgaard <joachim@loevgaard.dk>
2019-08-13 10:10:56 +0200

    simplified ProductVariantTraitSpec by using an anonymous class

---
+++ spec/Model/ProductVariantTraitSpec.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace spec\Loevgaard\SyliusBarcodePlugin\Model;
+
+use DateTimeInterface;
+use Loevgaard\SyliusBarcodePlugin\Model\BarcodeAwareInterface;
+use Loevgaard\SyliusBarcodePlugin\Model\ProductVariantTrait;
+use PhpSpec\ObjectBehavior;
+
+class ProductVariantTraitSpec extends ObjectBehavior
+{
+    public function let(): void
+    {
+        $class = new class implements BarcodeAwareInterface {
+            use ProductVariantTrait;
+        };
+
+        $this->beAnInstanceOf(get_class($class));
+    }
+
+    public function it_implements_barcode_aware_interface(): void
+    {
+        $this->shouldImplement(BarcodeAwareInterface::class);
+    }
+
+    public function it_marks_barcode_as_checked(): void
+    {
+        $this->markBarcodeAsChecked(true);
+        $this->isBarcodeChecked()->shouldReturn(true);
+    }
+
+    public function it_sets_marked_timestamp(): void
+    {
+        $this->markBarcodeAsChecked(true);
+        $this->getBarcodeChecked()->shouldReturnAnInstanceOf(DateTimeInterface::class);
+    }
+
+    public function it_marks_barcode_as_valid(): void
+    {
+        $this->markBarcodeAsChecked(true);
+        $this->isBarcodeValid()->shouldReturn(true);
+    }
+}
