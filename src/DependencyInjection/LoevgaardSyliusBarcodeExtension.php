d9713f5946268919ea2fd6f2bd1cab530d6bb739
Igor Mukhin <igor.mukhin@gmail.com>
2020-07-21 14:08:09 +0300

    Added: Barcode uniqueness configuration

--- src/DependencyInjection/LoevgaardSyliusBarcodeExtension.php
+++ src/DependencyInjection/LoevgaardSyliusBarcodeExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Loevgaard\SyliusBarcodePlugin\DependencyInjection;
 
-use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
@@ -12,16 +11,12 @@ use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class LoevgaardSyliusBarcodeExtension extends Extension
 {
-    /**
-     * {@inheritdoc}
-     *
-     * @throws Exception
-     */
     public function load(array $config, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $config);
         $container->setParameter('loevgaard_sylius_barcode.form.require', $config['form']['require']);
         $container->setParameter('loevgaard_sylius_barcode.form.require_valid', $config['form']['require_valid']);
+        $container->setParameter('loevgaard_sylius_barcode.form.require_unique', $config['form']['require_unique']);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
---
+++ src/EventListener/AddBarcodeUniqueSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Loevgaard\SyliusBarcodePlugin\EventListener;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
+use Doctrine\ORM\Events;
+use Loevgaard\SyliusBarcodePlugin\Model\BarcodeAwareInterface;
+
+final class AddBarcodeUniqueSubscriber implements EventSubscriber
+{
+    /** @var bool */
+    private $uniqueEnabled;
+
+    public function __construct(bool $uniqueEnabled)
+    {
+        $this->uniqueEnabled = $uniqueEnabled;
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::loadClassMetadata,
+        ];
+    }
+
+    public function loadClassMetadata(LoadClassMetadataEventArgs $event): void
+    {
+        $metadata = $event->getClassMetadata();
+
+        if (!is_subclass_of($metadata->name, BarcodeAwareInterface::class, true)) {
+            return;
+        }
+
+        if (!$this->uniqueEnabled) {
+            return;
+        }
+
+        $metadata->fieldMappings['barcode']['unique'] = true;
+    }
+}
