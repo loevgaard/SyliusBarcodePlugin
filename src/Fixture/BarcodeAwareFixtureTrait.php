5a3b2705e1cdd8b1b55fd6c37851f68802599cce
Igor Mukhin <igor.mukhin@gmail.com>
2020-09-16 12:53:13 +0300

    Added: Missed comment about purpose

--- src/Fixture/BarcodeAwareFixtureTrait.php
+++ src/Fixture/BarcodeAwareFixtureTrait.php
@@ -6,6 +6,10 @@ namespace Loevgaard\SyliusBarcodePlugin\Fixture;
 
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
+/**
+ * It can be used on some custom ProductVariantFixture
+ * (which Sylius don't have)
+ */
 trait BarcodeAwareFixtureTrait
 {
     protected function configureChannelResourceNode(ArrayNodeDefinition $resourceNode): void
